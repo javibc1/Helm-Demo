06198c1dae63977ac9f510fdd36caa5f995fa4e6
javibc1 <javibc1@hotmail.com>
2021-03-02 13:57:43 +0100

    fix error and add sleeps

--- argocd-namespace-isolation.sh
+++ argocd-namespace-isolation.sh
@@ -8,14 +8,17 @@ echo "Ruta = $ROUTE"
 
 oc new-project argocd-managed --skip-config-write
 
+sleep 5
+
 PASSWORD=$(oc get pod -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[*].metadata.name}')
 argocd --insecure --grpc-web login $ROUTE:443 --username "admin" --password $PASSWORD
 argocd cluster add $(oc config current-context) --name=argocd-managed --in-cluster --system-namespace=$NAMESPACE --namespace=argocd-managed
+
+sleep 5
 
 # Al añadir el clúster mediante la CLI, ésta se encarga de añadir los siguientes permisos
 # oc -n argocd create sa argocd-manager
 # oc create role -n argocd-managed argocd-manager-role --resource=*.*  --verb=*
 # oc create rolebinding argocd-manager-role-binding -n argocd-managed --role=argocd-manager-role --serviceaccount=$NAMESPACE:argocd-manager
 
-argocd app create nexus --repo=https://github.com/redhat-canada-gitops/catalog --path=nexus2/base \ 
-  --dest-server=https://kubernetes.default.svc --dest-namespace=argocd-managed --sync-policy=auto
+argocd app create nexus --repo=https://github.com/redhat-canada-gitops/catalog --path=nexus2/base --dest-server=https://kubernetes.default.svc --dest-namespace=argocd-managed --sync-policy=auto
