481067d2929bff964f643daf0eef40f9b2dc8755
javibc1 <javibc1@hotmail.com>
2021-03-30 14:05:31 +0200

    adding argocd-namespace-isolation

--- argocd-namespace-isolation.sh
+++ argocd-namespace-isolation.sh
@@ -7,18 +7,19 @@ ROUTE=$(oc get route argocd -o jsonpath='{ .spec.host }')
 echo "Ruta = $ROUTE"
 
 oc new-project argocd-managed --skip-config-write
-
-sleep 5
+oc new-project demo --skip-config-write
 
 PASSWORD=$(oc get pod -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[*].metadata.name}')
 argocd --insecure --grpc-web login $ROUTE:443 --username "admin" --password $PASSWORD
-argocd cluster add $(oc config current-context) --name=argocd-managed --in-cluster --system-namespace=$NAMESPACE --namespace=argocd-managed
+# array=("argocd-managed" "demo")
+# argocd cluster add $(oc config current-context) --name=cluster1 --in-cluster --system-namespace=$NAMESPACE --namespace=${array[@]}
 
-sleep 5
+argocd cluster add $(oc config current-context) --name=cluster1 --in-cluster --system-namespace=$NAMESPACE --namespace=argocd-managed --namespace=demo
 
 # Al añadir el clúster mediante la CLI, ésta se encarga de añadir los siguientes permisos
 # oc -n argocd create sa argocd-manager
-# oc create role -n argocd-managed argocd-manager-role --resource=*.*  --verb=*
-# oc create rolebinding argocd-manager-role-binding -n argocd-managed --role=argocd-manager-role --serviceaccount=$NAMESPACE:argocd-manager
+# oc create role argocd-manager-role --resource=*.*  --verb=*
+# oc create rolebinding argocd-manager-role-binding --role=argocd-manager-role --serviceaccount=$NAMESPACE:argocd-manager
 
-argocd app create nexus --repo=https://github.com/redhat-canada-gitops/catalog --path=nexus2/base --dest-server=https://kubernetes.default.svc --dest-namespace=argocd-managed --sync-policy=auto
+argocd app create nexus --repo=https://github.com/redhat-canada-gitops/catalog --path=nexus2/base --dest-server=https://kubernetes.default.svc --dest-namespace=demo --sync-policy=auto
+argocd app create nexus2 --repo=https://github.com/redhat-canada-gitops/catalog --path=nexus2/base --dest-server=https://kubernetes.default.svc --dest-namespace=argocd-managed --sync-policy=auto
